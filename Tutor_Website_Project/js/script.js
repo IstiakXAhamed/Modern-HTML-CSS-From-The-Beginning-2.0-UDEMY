f22c68e2fabc7278e07a8a664b6765ef9c298c3e
Sanim <sanim1728@gmail.com>
2026-07-27 15:55:58 +0600

    Add initial website structure and functionality
    - Created index.html with navigation, hero section, learn section, and course summary.
    - Added CSS links for styling and Font Awesome icons.
    - Implemented mobile menu toggle functionality in script.js.
    - Included scroll effect for navbar background color change.
    - Added images for testimonials and course descriptions.

---
+++ Tutor_Website_Project/js/script.js
@@ -0,0 +1,20 @@
+//toggle mobile menu
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+
+    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+
+    toggleButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+    });
+});
+
+//change navbar background color on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+    }
+});
